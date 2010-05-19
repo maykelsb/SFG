5fa23d32d57cf8aa2a40fb29e4a6f3f7c68bcea1
Maykel Santos Braz <maykelsb@yahoo.com.br>
2010-05-19 01:18:49 +0000

    Renomeando classes com o prefixo do projeto.

--- src/br/com/upzone/game/sfg/midlet/SFG_Midlet.java
+++ src/br/com/upzone/game/sfg/midlet/SFG_Midlet.java
@@ -4,9 +4,9 @@
  */
 package br.com.upzone.game.sfg.midlet;
 
-import br.com.upzone.gjme.tela.Tela;
+import br.com.upzone.gjme.tela.GjME_Tela;
 
-import br.com.upzone.game.sfg.tela.game.TelaGame;
+import br.com.upzone.game.sfg.tela.game.GjME_TelaGame;
 
 import javax.microedition.lcdui.Display;
 import javax.microedition.midlet.MIDlet;
@@ -15,15 +15,15 @@ import javax.microedition.midlet.MIDlet;
  *
  * @author Maykel "Gardner" dos Santos braz <maykelsb@yahoo.com.br>
  */
-public class SFGMidlet extends MIDlet {
+public class SFG_Midlet extends MIDlet {
 
-  private Tela tela;
+  private GjME_Tela tela;
   private Display display;
 
   public void startApp() {
     try {
       this.display = Display.getDisplay(this);
-      this.tela = new TelaGame();
+      this.tela = new GjME_TelaGame();
       this.tela.start();
       this.display.setCurrent(this.tela);
     } catch (Exception ex) {
