0fcb43a79a6d55c7f6fe041c1d212e2fcf41c0a2
gardner <gardner@db86b8a3-96c8-4283-991b-428d27aa4f91>
2010-06-05 12:05:02 +0000

    Renomeação de SFG_PersonagemJogador para SFG_Guile; Implementação do controle de comandos do personagem jogador;

--- trunk/src/br/com/upzone/game/sfg/personagem/SFG_Guile.java
+++ trunk/src/br/com/upzone/game/sfg/personagem/SFG_Guile.java
@@ -4,52 +4,51 @@
  */
 package br.com.upzone.game.sfg.personagem;
 
-import br.com.upzone.gjme.GjME_Direcoes;
+import br.com.upzone.gjme.GjME_Fisica;
 import br.com.upzone.gjme.personagem.GjME_Personagem;
 import br.com.upzone.gjme.personagem.GjME_IPersonagemControlavel;
-import br.com.upzone.gjme.personagem.estado.GjME_Estado;
+import br.com.upzone.gjme.personagem.acao.GjME_Acao;
 
 import java.io.IOException;
 
 import javax.microedition.lcdui.Image;
 import javax.microedition.lcdui.game.GameCanvas;
+import javax.microedition.lcdui.game.LayerManager;
 
 /*
  *
  * @author Maykel "Gardner" dos Santos braz <maykelsb@yahoo.com.br>
  */
-public class SFG_PersonagemJogador extends GjME_Personagem
-        implements GjME_IPersonagemControlavel {
+public class SFG_Guile extends GjME_Personagem
+                       implements GjME_IPersonagemControlavel {
 
-  public final static int EST_PERSON_ANDANDO = 1;
-  public final static int EST_PERSON_VIRANDO = 2;
+  public final static int ACAO_ANDAR = 1;
+  public final static int ACAO_VIRAR = 2;
   public final static int EST_PERSON_ABAIXANDO = 3;
   public final static int EST_PERSON_PULANDO = 4;
   public final static int EST_PERSON_CORRENDO = 5;
   public final static int EST_PERSON_LEVANTANDO = 6;
   public final static int EST_PERSON_SOCO_FRACO = 7;
 
-  public SFG_PersonagemJogador() throws IOException {
-    super(Image.createImage("/SpriteSheets/guile.png"), 51, 50, 10, 100,
-            170, 220, 10);
+  public SFG_Guile() throws IOException {
+    super(Image.createImage("/SpriteSheets/guile.png"), 51, 50, 10, 100, 170, 120);
     this.bRefletirSprite = true;
 
-    // -- Adição do estado PARADO
-    this.addEstado(SFG_PersonagemJogador.EST_PERSON_PARADO,
-      new GjME_Estado(this, SFG_PersonagemJogador.EST_PERSON_PARADO, 0, 5) {
-        public void executeNoEstado() {}});
+    // -- Ações do personagem --------------------------------------------------
+    this.adicionarAcao(SFG_Guile.ACAO_AGUARDAR,
+      new GjME_Acao(0, 5) {
+        public void executar(GjME_Personagem prg) { }
+    });
+    this.adicionarAcao(SFG_Guile.ACAO_ANDAR,
+      new GjME_Acao(7, 11) {
+        public void executar(GjME_Personagem prg) { }
+      });
+    this.adicionarAcao(SFG_Guile.ACAO_VIRAR,
+      new GjME_Acao(6, 6) {
+        public void executar(GjME_Personagem prg) { }
+      }.defineTipoAnimacao(GjME_Acao.ANIMACAO_NAO_CONTINUA));
 
-    // -- Adição do estado ANDANDO
-    this.addEstado(SFG_PersonagemJogador.EST_PERSON_ANDANDO,
-      new GjME_Estado(this, SFG_PersonagemJogador.EST_PERSON_ANDANDO, 7, 11) {
-        public void executeNoEstado() {
-          this.personagem.deslocarNaHorizontal();
-        }});
-
-    // -- Adição do estado VIRANDO
-    this.addEstado(SFG_PersonagemJogador.EST_PERSON_VIRANDO,
-      new GjME_Estado(this, SFG_PersonagemJogador.EST_PERSON_VIRANDO, 6, 6) {
-        public void executeNoEstado() {}});
+/*
 
     // -- Adição do estado ABAIXANDO
     this.addEstado(SFG_PersonagemJogador.EST_PERSON_ABAIXANDO,
@@ -65,7 +64,7 @@ public class SFG_PersonagemJogador extends GjME_Personagem
        .setPosEstado(SFG_PersonagemJogador.EST_PERSON_PARADO));
 
     // -- Adição do estado PULANDO
-    this.addEstado(SFG_PersonagemJogador.EST_PERSON_PULANDO,
+/*    this.addEstado(SFG_PersonagemJogador.EST_PERSON_PULANDO,
       new GjME_Estado(this, SFG_PersonagemJogador.EST_PERSON_PULANDO, new int[] {12, 21, 21, 22, 22, 22, 22, 21, 21, 12}) {
         public void executeNoEstado() {
           this.personagem.deslocarNaVertical();
@@ -74,25 +73,41 @@ public class SFG_PersonagemJogador extends GjME_Personagem
      .setPosEstado(SFG_PersonagemJogador.EST_PERSON_PARADO));
 
     // -- Adição do estado CORRENDO
-    this.addEstado(SFG_PersonagemJogador.EST_PERSON_CORRENDO,
+/*    this.addEstado(SFG_PersonagemJogador.EST_PERSON_CORRENDO,
       new GjME_Estado(this, SFG_PersonagemJogador.EST_PERSON_CORRENDO, new int[] {14, 15, 16, 17}) {
         public void executeNoEstado() {
           this.personagem.deslocarNaHorizontal(5);
         }
-    });
+    });*/
 
-    // -- Adição do estado SOCO_FRACO
+  /*
+            // -- Adição do estado SOCO_FRACO
     this.addEstado(SFG_PersonagemJogador.EST_PERSON_SOCO_FRACO,
       new GjME_Estado(this, SFG_PersonagemJogador.EST_PERSON_SOCO_FRACO, new int[] {2, 23, 24, 23}) {
         public void executeNoEstado() {}
     }.defineContinuidade(GjME_Estado.TIP_EST_NAO_CONTINUO)
-     .setPosEstado(SFG_PersonagemJogador.EST_PERSON_PARADO));
+     .setPosEstado(SFG_PersonagemJogador.EST_PERSON_PARADO));/*/
 
     // -- Definindo o Estado inicial
-    this.setEstado(SFG_PersonagemJogador.EST_PERSON_PARADO);
+    this.empilharAcao(SFG_Guile.ACAO_AGUARDAR);
   }
 
-  public void processarInput(int iKeyState) {
+  public void processarInput(int iKeyState, LayerManager lm) {
+    switch (((GjME_Acao)this.stkAcoes.peek()).retornaIDAcao()) {
+      case SFG_Guile.ACAO_AGUARDAR:
+      case SFG_Guile.ACAO_ANDAR:
+        if ((iKeyState & GameCanvas.RIGHT_PRESSED) != 0) {
+          this.empilharAcao(SFG_Guile.ACAO_ANDAR);
+          this.andar(GjME_Fisica.DIREITA);
+        } else if ((iKeyState & GameCanvas.LEFT_PRESSED) != 0) {
+          this.empilharAcao(SFG_Guile.ACAO_ANDAR);
+          this.andar(GjME_Fisica.ESQUERDA);
+        } else { this.desempilharAcoes(); }
+        break;
+      default: this.desempilharAcoes();
+    }
+    ((GjME_Acao)this.stkAcoes.peek()).executar(this);
+/*
     if (this.iEstadoAtual == SFG_PersonagemJogador.EST_PERSON_PULANDO) {
       
     } else if (this.iEstadoAtual == SFG_PersonagemJogador.EST_PERSON_SOCO_FRACO) {
@@ -100,31 +115,37 @@ public class SFG_PersonagemJogador extends GjME_Personagem
     } else {
       if ((iKeyState & GameCanvas.LEFT_PRESSED) != 0) {
         this.setEstado(SFG_PersonagemJogador.EST_PERSON_ANDANDO);
-        this.setDirecaoPersonagem(GjME_Direcoes.ESQUERDA);
+        this.setDirecaoPersonagem(GjME_Fisica.ESQUERDA);
       } else if ((iKeyState & GameCanvas.DOWN_PRESSED) != 0) {
         this.setEstado(SFG_PersonagemJogador.EST_PERSON_ABAIXANDO);
       } else if ((iKeyState & GameCanvas.UP_PRESSED) != 0) {
         this.setEstado(SFG_PersonagemJogador.EST_PERSON_PULANDO);
       } else if ((iKeyState & GameCanvas.RIGHT_PRESSED) != 0) {
         this.setEstado(SFG_PersonagemJogador.EST_PERSON_CORRENDO);
-        this.setDirecaoPersonagem(GjME_Direcoes.DIREITA);
+        this.setDirecaoPersonagem(GjME_Fisica.DIREITA);
       } else if ((iKeyState & GameCanvas.FIRE_PRESSED) != 0) {
         this.setEstado(SFG_PersonagemJogador.EST_PERSON_SOCO_FRACO);
       } else {
         this.setEstado(SFG_PersonagemJogador.EST_PERSON_PARADO);
       }
     }
-    this.estadoAtual.executeNoEstado();
+    this.estadoAtual.executeNoEstado();*/
   }
 
-  public void deslocarNaVertical() {
-    switch (this.getFrame()) {
-      case 1: case 2: case 3: case 4:
-        this.setPosition(this.getX(), (this.getY() - 10));
-        break;
-      case 5: case 6: case 7: case 8:
-        this.setPosition(this.getX(), (this.getY() + 10));
-        break;
+  private void andar(int direcao) {
+    if (GjME_Fisica.DIREITA == direcao) {
+      if (GjME_Fisica.ESQUERDA == this.direcaoPersonagem) {
+        this.direcaoPersonagem(GjME_Fisica.DIREITA);
       }
+      this.iVelX = GjME_Fisica.VELOCIDADE_MEDIA;
+    } else if (GjME_Fisica.ESQUERDA == direcao) {
+      if (GjME_Fisica.DIREITA == this.direcaoPersonagem) {
+        this.direcaoPersonagem(GjME_Fisica.ESQUERDA);
+      }
+      this.iVelX = -GjME_Fisica.VELOCIDADE_MEDIA;
+    }
+    if (SFG_Guile.ACAO_ANDAR != ((GjME_Acao)this.stkAcoes.peek()).retornaIDAcao()){
+      this.desempilharAcoes();
+    }
   }
 }
